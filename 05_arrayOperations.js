cec983731a04001fcd84c8b920a4be57caa3013b
Sophie Coulet <coulet.so@gmail.com>
2024-06-10 12:14:50 +0200

    correction exo 05

--- 05_arrayOperations.js
+++ 05_arrayOperations.js
@@ -58,7 +58,7 @@ console.log(sum([4,2,5,1]));
  *   - Vous ne pouvez pas utiliser de variable (autre que l'argument de la fonction)
   */
 
-const findUserById = (array, id) => array.find(item => item.id === id);
+const findUserById = (array, id) => array.find(item => item.id === id).name;
 console.log(findUserById([{id: 1, name: 'John'},{id: 2, name: 'Doe'},{id: 3, name: 'Foo'},{id: 4, name: 'Bar'}], 2));
 
 module.exports = {multiplyByTwo, filterNameStartByA, sum, findUserById};
